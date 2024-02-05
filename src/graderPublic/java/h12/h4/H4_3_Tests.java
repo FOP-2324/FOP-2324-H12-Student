6e1d3892b0fb2f19f2c153d172d114e4a069f68e
nyanyan <zentox@users.noreply.github.com>
2024-02-05 15:38:23 +0100

    Remove private test class

--- src/graderPublic/java/h12/h4/H4_3_Tests.java
+++
@@ -1,8 +0,0 @@
-package h12.h4;
-
-import org.sourcegrade.jagr.api.rubric.TestForSubmission;
-
-@TestForSubmission()
-public class H4_3_Tests {
-    // TODO: No idea how to test this without depending on previous tasks
-}
